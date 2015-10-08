0d7707b8e634827d2eb6dea7ead74558afd23473
Antonio Spinelli <tonicospinelli85@gmail.com>
2015-10-08 14:52:40 -0300

    fix broken Resources tests

--- tests/Unit/Component/ResourceToolTest.php
+++ tests/Unit/Component/ResourceToolTest.php
@@ -6,19 +6,34 @@ namespace QueueTest\Unit\Component;
 use Queue\Component\ResourceTool;
 use Queue\Component\Setup;
 use Queue\Configuration;
+use Queue\Connection;
 use Queue\Driver;
 use Queue\DriverManager;
 
 class ResourceToolTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testCreateYAMLResource()
+    public function testRaiseAmqpResourcesSoThatDoesNotThrowAnException()
     {
-        $connection = DriverManager::getConnection(
-            new Configuration(Driver::AMQP, RABBIT_HOST, RABBIT_PORT, RABBIT_USERNAME, RABBIT_PASSWORD)
-        );
-        $resourceManager = Setup::createYAMLResource(__DIR__ . '/resources.yml', $connection->getConfiguration());
+        $connection = $this->getMockBuilder('Queue\Driver\Connection')->getMock();
+
+        $driver = $this
+            ->getMockBuilder('Queue\Driver')
+            ->setMethods(array('connect'))
+            ->getMock();
+        $driver->expects($this->once())->method('connect')->willReturn($connection);
+        $configuration = new Configuration(Driver::AMQP, RABBIT_HOST, RABBIT_PORT, RABBIT_USERNAME, RABBIT_PASSWORD);
+
+        $resourceManager = Setup::createYAMLResource(__DIR__ . '/resources.yml', $configuration);
+
+        $connection = new Connection($configuration, $driver);
+
         $tool = new ResourceTool($connection, $resourceManager);
+
         $tool->raiseResources();
+
+        // I know it is weird, but if you not agree with it,
+        // do your best and show me how to solve it! ;)
+        $this->assertTrue(true);
     }
 }
--- tests/Unit/Component/SetupTest.php
+++ tests/Unit/Component/SetupTest.php
@@ -13,13 +13,18 @@ class SetupTest extends \PHPUnit_Framework_TestCase
     {
         $resourceManager = Setup::createYAMLResource(__DIR__ . '/resources.yml', new Configuration(Driver::AMQP));
 
-        $this->assertInstanceOf('Queue\Resources\Queue', $resourceManager->getQueue('log.error'));
-        $this->assertInstanceOf('Queue\Resources\Queue', $resourceManager->getQueue('log.something'));
+        $this->assertInstanceOf('Queue\Resources\Queue', $resourceManager->getQueue('logs.error'));
+        $this->assertInstanceOf('Queue\Resources\Queue', $resourceManager->getQueue('logs.something'));
 
-        $tunnel = $resourceManager->getTunnel('log');
+        $tunnel = $resourceManager->getTunnel('logs');
+
         $this->assertInstanceOf('Queue\Resources\Tunnel', $tunnel);
-        $this->assertCount(1, $tunnel->getRoutes());
+        $this->assertCount(2, $tunnel->getRoutes());
+
         $this->assertTrue($tunnel->hasRoute('error'));
         $this->assertCount(2, $tunnel->getQueuesFromRoute('error'));
+
+        $this->assertTrue($tunnel->hasRoute('warning'));
+        $this->assertCount(1, $tunnel->getQueuesFromRoute('warning'));
     }
 }
